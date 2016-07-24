e6602f09178edf7c232f13fe567d2f9895ea47a4
Chris Cornell <ccornell@gmail.com>
2016-07-23 22:20:24 -0700

    Added mathfu to externals and the include path.
    Also did some minor cleanup in main.  Needs more, but
    this is a start.

--- telegram/src/main.cpp
+++ telegram/src/main.cpp
@@ -54,6 +54,21 @@ int main(int argc, char* args[])
 			// Once finished with OpenGL functions, the SDL_GLContext can be deleted.
 			//SDL_GL_DeleteContext(glcontext);
 
+			// Set our OpenGL version.
+			// SDL_GL_CONTEXT_CORE gives us only the newer version, deprecated functions are disabled
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+			// 3.2 is part of the modern versions of OpenGL, but most video cards whould be able to run it
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+			// Turn on double buffering with a 24bit Z buffer.
+			// You may need to change this to 16 or 32 for your system
+			SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+			// This makes our buffer swap syncronized with the monitor's vertical refresh
+			SDL_GL_SetSwapInterval(1);
+
 
 
 
